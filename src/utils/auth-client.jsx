46cfc82a7b2152e86e7b777dfeb05d7a5c2e858c
mouislambd <tanhaislammou097@gmail.com>
2026-06-21 19:40:30 -0700

    auth-client file problem solving

--- src/utils/auth-client.jsx
+++ src/utils/auth-client.jsx
@@ -1,7 +1,7 @@
 import { createAuthClient } from "better-auth/react";
 
 export const authClient = createAuthClient({
-    baseURL: "https://bibliodrop-server.onrender.com",
+    baseURL: import.meta.env.VITE_BETTER_AUTH_URL || "https://bibliodrop-server.onrender.com",
 });
 
 export const { signIn, signUp, signOut, useSession } = authClient;
